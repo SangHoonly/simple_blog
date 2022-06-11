5a3f892bb2fb96a945c1f3e19d894899b29a28ed
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-06-11 16:01:26 +0900

    프로젝트 초기화

---
+++ src/main/java/simple_blog/LeeJerry/LeeJerryApplication.java
@@ -0,0 +1,13 @@
+package simple_blog.LeeJerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeJerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeJerryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simple_blog/LeeJerry/LeeJerryApplicationTests.java
@@ -0,0 +1,13 @@
+package simple_blog.LeeJerry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeJerryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
